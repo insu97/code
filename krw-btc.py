139cd29b45739da65dc41b24dd9989c834a1a7ee
insu97 <52162653+insu97@users.noreply.github.com>
2022-11-13 01:12:47 +0900

    Update krw-btc.py

--- krw-btc.py
+++ krw-btc.py
@@ -3,23 +3,17 @@ import pyupbit
 import time
 import requests
 
-access = "Access key"
-secret = "Secret key"
+access = ""
+secret = ""
 upbit = pyupbit.Upbit(access, secret)
 
 buy_count = 0
 buy_money = 5000
 
-slack_token = 'bot_tokenn'
+slack_token = ''
 client = slack_sdk.WebClient(token=slack_token)
 
 while (upbit.get_balance('KRW') > (buy_money * 1.0006)):
-
-    for i in range(len(upbit.get_balances())):
-        if upbit.get_balances()[i]['currency'] == 'BTC':
-            # BTC의 평균가
-            avg_price = upbit.get_balances()[i]['avg_buy_price']
-            break
 
     # KRW-BTC가 0원이면 시장가 매수
     if upbit.get_balance("KRW-BTC") == 0:
@@ -35,6 +29,12 @@ while (upbit.get_balance('KRW') > (buy_money * 1.0006)):
         client.chat_postMessage(channel='upbit', text=message)
 
         time.sleep(5)
+
+    for i in range(len(upbit.get_balances())):
+        if upbit.get_balances()[i]['currency'] == 'BTC':
+            # BTC의 평균가
+            avg_price = upbit.get_balances()[i]['avg_buy_price']
+            break
 
     url = "https://api.upbit.com/v1/trades/ticks?market=KRW-BTC&count=1"
     headers = {"accept": "application/json"}
